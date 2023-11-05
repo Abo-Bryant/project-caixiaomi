8680a783bcda1a7a798d5dbddcdf6a45249137fb
lb <484532503@qq.com>
2023-11-05 16:00:26 +0800

    three

--- src/goodsAdd/goods_add/kind_manage.vue
+++ src/goodsAdd/goods_add/kind_manage.vue
@@ -9,7 +9,7 @@
           }}种)</text>
         </view>
         <view slot="value">
-          <u-icon name="trash-fill" color="#eb2503" size="28" @click="doDel"></u-icon>
+          <u-icon name="trash-fill" color="#eb2503" size="28" @click="doDel(item)"></u-icon>
         </view>
       </u-cell>
     </u-cell-group>
@@ -27,10 +27,19 @@
     </view>
     <!-- 修改分类 -->
     <view class="goods-add">
-      <u-modal :show="addshow" title="修改分类" showCancelButton @cancel="amendCancel" @confirm="amendConfirm">
+      <u-modal :show="amendshow" title="修改分类" showCancelButton @cancel="amendCancel" @confirm="amendConfirm">
         <view class="slot-content">
           <!-- <rich-text :nodes="'123456'"></rich-text> -->
           <u--input placeholder="请输入内容" border="surround" v-model="valueAmend"></u--input>
+        </view>
+      </u-modal>
+    </view>
+    <!-- 删除弹出框 -->
+    <view class="goods-del">
+      <u-modal :show="delshow" showCancelButton confirmText="删除" confirmColor="#eb370c" @cancel="delCancel"
+        @confirm="delConfirm">
+        <view class="slot-content">
+          <rich-text :nodes="`确定删除这个货品吗？`"></rich-text>
         </view>
       </u-modal>
     </view>
@@ -42,11 +51,20 @@ export default {
   name: '',
   data () {
     return {
+      amendshow: false,
+      // 删除时那一行的数据
+      delItem: {},
+      // 控制删除弹出框
+      delshow: false,
+      // 修改时用到的id
       idAmend: '',
+      // 修改时用到的vaule
       valueAmend: '',
       categoriesList: [],
       leftList: [],
+      // 控制新增弹出框
       addshow: false,
+      // 新增时用到的value
       valueAdd: '',
     }
   },
@@ -61,20 +79,24 @@ export default {
       this.categoriesList = uni.getStorageSync('categoriesList')
       // console.log(this.categoriesList);
       this.leftList = this.categoriesList.filter((item, index) => {
-        return index !== 0
+        if (item.goodsKind === '未分类') {
+          return index !== 0 && index !== this.categoriesList.length - 1
+        } else {
+          return index !== 0
+        }
       })
       console.log(this.leftList);
     },
     // 点击修改
     doAmend (goodsKind, id) {
-      console.log('修改', goodsKind);
-      this.addshow = true
+      // console.log('修改', goodsKind);
+      this.amendshow = true
       this.valueAmend = goodsKind
       this.idAmend = id
     },
     // 修改弹出框的取消
     amendCancel () {
-      this.addshow = false
+      this.amendshow = false
     },
     // 修改弹出框的确认
     amendConfirm () {
@@ -90,13 +112,77 @@ export default {
           item.goodsKind = this.valueAmend
         }
       })
-      console.log(this.categoriesList);
+      // console.log(this.categoriesList);
       uni.setStorageSync('categoriesList', this.categoriesList)
-      this.addshow = false
+      this.amendshow = false
     },
     // 点击删除的icon
-    doDel () {
-      console.log('删除');
+    doDel (item) {
+      this.delItem = item
+      // console.log('删除', item);
+      if (item.children.length > 0) {
+        this.delshow = true
+      } else {
+        // console.log('没有货品');
+        this.leftList = this.leftList.filter(i => {
+          // console.log(i, item, i.goodsKind, item.goodsKind);
+          if (i.goodsKind !== item.goodsKind) {
+            return i
+          }
+        })
+        // console.log(this.leftList);
+        this.categoriesList = this.categoriesList.filter(i => {
+          // console.log(i, item, i.goodsKind, item.goodsKind);
+          if (i.goodsKind !== item.goodsKind) {
+            return i
+          }
+        })
+        uni.setStorageSync('categoriesList', this.categoriesList)
+      }
+
+    },
+    // 点击删除的取消
+    delCancel () {
+      this.delshow = false
+    },
+    // 点击删除的确认
+    delConfirm () {
+      console.log(this.delItem);
+      const delData = this.leftList.filter(i => {
+        // console.log(i, item, i.goodsKind, item.goodsKind);
+        if (i.goodsKind === this.delItem.goodsKind) {
+          return i
+        }
+      })
+      // const delDataItem = delData.children.map(item => {
+      //   return item
+      // })
+      console.log('delData,', delData[0].children);
+      this.categoriesList = this.categoriesList.filter(i => {
+        // console.log(i, item, i.goodsKind, item.goodsKind);
+        if (i.goodsKind !== this.delItem.goodsKind) {
+          return i
+        }
+      })
+      // console.log('打印', );
+      // this.categoriesList=this.categoriesList.map(item => {
+      //   // console.log('打印', item);
+      //   if(item.goodsKind==='未分类'){
+      //     return 
+      //   }
+      // })
+
+      this.categoriesList = [...this.categoriesList, { goodsKind: '未分类', id: String(this.categoriesList.length), pid: '', children: delData[0].children }]
+      uni.setStorageSync('categoriesList', this.categoriesList)
+      this.leftList = this.leftList.filter(i => {
+        // console.log(i, item, i.goodsKind, item.goodsKind);
+        if (i.goodsKind !== this.delItem.goodsKind) {
+          return i
+        }
+      })
+
+      this.delshow = false
+
     },
     // 点击新增
     doAdd () {
@@ -105,17 +191,22 @@ export default {
     },
     // 新增弹出框的取消
     addCancel () {
+      console.log('取消');
       this.addshow = false
     },
     // 新增弹窗框的确认
     addConfirm () {
-      // console.log(this.valueAdd);
-      this.addshow = false
+      console.log('确认');
+      console.log(this.categoriesList);
+      console.log(this.valueAdd);
+
       this.goodsKind = this.valueAdd
+
       this.leftList.push({ goodsKind: this.valueAdd, id: String(this.categoriesList.length), pid: '', children: [] })
       this.categoriesList = [...this.categoriesList, { goodsKind: this.valueAdd, id: String(this.categoriesList.length), pid: '', children: [] }]
-      // console.log(this.categoriesList);
       uni.setStorageSync('categoriesList', this.categoriesList)
+      this.valueAdd = ''
+      this.addshow = false
     },
   },
 }
